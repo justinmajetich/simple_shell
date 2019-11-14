c5ae3d958b87aefcec0a6a7d0b890dbb4e3c786c
justinmajetich <justinmajetich@gmail.com>
2019-11-14 21:59:12 +0000

    Create parent function

---
+++ shell.c
@@ -0,0 +1,48 @@
+#include "shell.h"
+
+/**
+ * _getline - 
+ *
+ *
+ */
+ssize_t _getline(char *line, size_t size, FILE *input)
+{
+
+}
+
+/**
+ * _strtok -
+ *
+ *
+ */
+char *_strtok(char *line)
+{
+}
+
+/**
+ *
+ *
+ */
+_execute(char *tok)
+{
+	execve();
+}
+
+/**
+ *
+ *
+ *
+ */
+void main(void)
+{
+	/* print command prompt */
+
+	/* read command line */
+	_getline()
+
+	/* parse command line */
+	_strtok();
+
+	/* execute command line */
+	_execute();
+}
