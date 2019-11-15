16181c5168a84845768bc40abfd7d0fed80e3338
justinmajetich <justinmajetich@gmail.com>
2019-11-15 02:31:51 +0000

    Build out _getline function

--- shell.c
+++ shell.c
@@ -1,13 +1,24 @@
 #include "shell.h"
 
 /**
- * _getline - 
- *
+ * _getline - retrieve command line
+ * @line: buffer to store command line
+ * @size: length of command line
  *
  */
-ssize_t _getline(char *line, size_t size, FILE *input)
+ssize_t _getline(char **line)
 {
+	ssize_t read_cnt;
 
+	/* read stdin to dynamic buffer */
+	read_cnt = read(STDIN_FILENO, *line, ARG_MAX);
+	if (read_cnt < 0) /* check for read failure */
+		return (-1);
+
+	(*line)[read_cnt] = '\0'; /* null-terminate */
+
+	/* return total number of bytes read */
+	return (read_cnt);
 }
 
 /**
@@ -35,10 +46,16 @@ _execute(char *tok)
  */
 void main(void)
 {
+	char *line = malloc(sizeof(char) * ARG_MAX);
+
+	/* check for allocation fail */
+	if (!line)
+		perror("Failed to allocate buffer");
+
 	/* print command prompt */
 
 	/* read command line */
-	_getline()
+	_getline(&line);
 
 	/* parse command line */
 	_strtok();
--- shell.h
+++ shell.h
@@ -1,6 +1,7 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+/* LIBRARIES */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
