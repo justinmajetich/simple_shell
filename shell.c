3baa983d061dc437533e26fdc1da6edd5ebae252
justinmajetich <justinmajetich@gmail.com>
2019-11-19 19:26:43 +0000

    Move extern declarations for Betty stlye

--- shell.c
+++ shell.c
@@ -1,63 +1,9 @@
 #include "shell.h"
 /**
- * _getline - retrieve command line
- * @line: buffer to store command line
- * @size: length of command line
- *
- * Return: Number of lines read on success or -1 on fail
- */
-ssize_t _getline(char **line, size_t *line_size)
-{
-	ssize_t read_cnt, read_total = 0;
-	size_t offset = 0; /* line offset for each iteration */
-	char *buffer = NULL;
-
-	buffer = alloc_mngr(buffer, (sizeof(char) * 1024));
-
-	if (!buffer) /* error if malloc fail */
-	{
-		perror("Allocation failed");
-		return (-1);
-	}
-
-	/* read stdin to dynamic buffer */
-	while ((read_cnt = read(STDIN_FILENO, buffer, sizeof(buffer))) > 0)
-	{
-		if (read_cnt == -1) /* check for read failure */
-			return (-1);
-
-		if (read_cnt) /* if new text read */
-		{
-			read_total += read_cnt; /* add num of bytes last read to total */
-
-			/* reallocate line to receive new text */
-			*line = alloc_mngr(*line, (sizeof(char) * read_total));
-			if (!(*line)) /* check for reallocation fail */
-			{
-				perror("Reallocation failed");
-				return (-1);
-			}
-
-			/* copy buffer to line from current offset */
-			_strncpy(((*line) + offset), buffer, read_cnt);
-
-			offset = read_total; /* advance line offset */
-		}
-		if ((*line)[offset - 1] == '\n') /* if line is \n terminated */
-			break; /* break read loop */
-	}
-	(*line)[offset - 1] = '\0'; /* null-terminate line */
-
-	/* return total number of bytes read */
-	return (*line_size = offset);
-}
-
-/**
- * main - The main function called after command line input
+ * main - loop through essential shell input and execution tasks
  */
 int main(void)
 {
-	char **environ;
 	mem_list *mem_head;
 	char *line = NULL;
 	size_t line_size = 0;
