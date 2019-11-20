d84a7ac5c28464d8827b9064877d23762096a282
justinmajetich <justinmajetich@gmail.com>
2019-11-20 02:22:58 +0000

    Check for NULL tokens array

--- shell.c
+++ shell.c
@@ -1,30 +1,36 @@
 #include "shell.h"
 /**
  * main - loop through essential shell input and execution tasks
+ *
+ * Return: 0 or EXIT_SUCCESS on Sucess, EXIT_FAILURE on Failure
  */
 int main(void)
 {
 	char *line = NULL;
 	size_t line_size = 0;
 	char **tok_array = NULL;
-	int i = 0;
+	int loop = 1; /* loop = temp loop driver */
 
-	/* print command prompt */
-	write(1, "$ ", 2);
+	do {
+		/* print command prompt */
+		write(1, "$ ", 2);
 
-	/* read command line */
-	_getline(&line, &line_size);
+		/* read command line */
+		_getline(&line, &line_size);
 
-	/* set 2D array to parsed command line */
-	tok_array = _strtok(line);
+		/* set pointer array to parsed command line */
+		tok_array = _strtok(line);
+		
+		if (tok_array) /* if token present */
+			exec_mngr(tok_array); /* pass args to executor */
 
-	for (i = 0; tok_array[i] != NULL; i++)
-		printf("%s\n", tok_array[i]);
+		/* Free everything allocated */
+		/*free_mem_list(&mem_head);*/
 
-	/* INSERT EXECUTE FUNCTIONS */
-
-	/* Free everything allocated */
-	free_mem_list(&mem_head);
+	line = NULL;
+	tok_array = NULL;
+	
+	} while (loop);
 
 	return (0);
 }
