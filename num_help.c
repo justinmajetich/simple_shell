340b3df6951313be5ba52a40ca31131aa4cd93f9
Michelle Giraldo <michellegsld@gmail.com>
2019-11-24 22:38:23 -0500

    New function counting amount of digits in a number

--- num_help.c
+++ num_help.c
@@ -1,9 +1,11 @@
 #include "shell.h"
+
 /**
 * _itoa - Function that converts a number to string
 * @number: value to be converted to a string
 * @buffer: array that stores null-termintated string result
 * @base: value of the string
+*
 * Return: converted string
 */
 char *_itoa(size_t number, char *buffer, int base)
@@ -17,12 +19,14 @@ char *_itoa(size_t number, char *buffer, int base)
 		buffer[i++] = '0';
 		buffer[i] = '\0';
 	}
-	/* if negative, convert to pos and track sign
-	if (number < 0 && base == 10)
-	{
-		number *= -1;
-		sign *= -1;
-	} */
+	/*
+	 * if negative, convert to pos and track sign
+	 * if (number < 0 && base == 10)
+	 * {
+	 *	number *= -1;
+	 *	sign *= -1;
+	 * }
+	 */
 	/* if number is not 0 */
 	while (number)
 	{
@@ -38,4 +42,23 @@ char *_itoa(size_t number, char *buffer, int base)
 	if (number)
 		buffer[i] = '\0';
 	return (_revstr(buffer));
+}
+
+/**
+ * count_digit - Returns how many digits make up a number
+ * @num: The number to count the amount of digits
+ *
+ * Return: Always a size_t
+ */
+size_t count_digit(size_t num)
+{
+	size_t count = 0;
+
+	while (num != 0)
+	{
+		num /= 10;
+		count++;
+	}
+
+	return (count);
 }
